ae82900fcafa877f1a495eac5875655e356f361b
Fateme Rahmany <138863634+araaancode@users.noreply.github.com>
2025-08-13 01:25:23 +0330

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,9 +14,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://googlesheets-1-9526.onrender.com',
+    origin: 'https://googlesheets-1-9526.onrender.com', 
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials:true
+    credentials: true,
   })
 );
 
